4cf49f733bedd1baa277fdb6ba0397b06d029ad7
Saibyasachi Ruhan <127364592+SDRoan@users.noreply.github.com>
2025-04-06 19:27:45 -0400

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="AI Climate Impact Calculator", layout="centered")
 
 
 
-flan_tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+#flan_tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
 flan_model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
 
 
